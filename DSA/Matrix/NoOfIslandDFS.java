43ebf11f7c12c23bcf104196477d218f9fdae86c
Anurag Sinha <sinhaanurag18@gmail.com>
2020-07-31 10:14:14 +0530

    DFS solution of no of island

---
+++ DSA/Matrix/NoOfIslandDFS.java
@@ -0,0 +1,33 @@
+package DSA.Matrix;
+/**
+ * https://leetcode.com/problems/number-of-islands/
+ * Space Complexity : O(1)
+ * Time Complexity : O(n*m)
+ * we are entering each cell 2 times
+ */
+class NoOfIslandDFS {
+    public int numIslands(char[][] grid) {
+        if(grid.length==0||grid[0].length==0)
+            return 0;
+        int count=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    dfs(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+
+    public void dfs(char[][] grid,int x,int y){
+        if(x>=grid.length||x<0||y>=grid[0].length||y<0||grid[x][y]=='0')
+            return;
+        grid[x][y]='0';
+        dfs(grid,x-1,y);
+        dfs(grid,x+1,y);
+        dfs(grid,x,y-1);
+        dfs(grid,x,y+1);
+    }
+}
