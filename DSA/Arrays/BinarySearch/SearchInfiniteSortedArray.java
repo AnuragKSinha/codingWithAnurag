2ac5e44ca7b40c657b30d43a56aec191dbd9da0a
Anurag Sinha <sinhaanurag18@gmail.com>
2020-09-22 11:54:10 +0530

    logic of double

--- DSA/Arrays/BinarySearch/SearchInfiniteSortedArray.java
+++ DSA/Arrays/BinarySearch/SearchInfiniteSortedArray.java
@@ -2,12 +2,12 @@ package DSA.Arrays.BinarySearch;
 /**
  * Space Complexity:O(1)
  * Time Complexity:(log n)
- * Given an array of numbers sorted in ascending order, 
- * find the range of a given number ‘key’. The range of the ‘key’ 
- * will be the first and last position of the ‘key’ in the array.
- * Write a function to return the range of the ‘key’. 
- * If the ‘key’ is not present return [-1, -1].
- 
+ * Given an infinite sorted array (or an array with unknown size), 
+ * find if a given number ‘key’ is present in the array. Write a function to return the index of the ‘key’ 
+ * if it is present in the array, otherwise return -1.
+ * Since it is not possible to define an array with infinite (unknown) size, you will be provided with an 
+ * interface ArrayReader to read elements of the array. ArrayReader.get(index) will return the number at index; 
+ * if the array’s size is smaller than the index, it will return Integer.MAX_VALUE.
  */
 class SearchInfiniteSortedArray {
 class ArrayReader {
