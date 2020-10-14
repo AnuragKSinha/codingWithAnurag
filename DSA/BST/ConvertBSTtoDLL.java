a3fa9f904145a5bbe9bd9e0a2f54bb3acdd504f8
Anurag Sinha <sinhaanurag18@gmail.com>
2020-10-14 15:26:07 +0530

    Convert BST to Double LinkedList

---
+++ DSA/BST/ConvertBSTtoDLL.java
@@ -0,0 +1,53 @@
+package DSA.BST;
+/**
+ * https://leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list/
+ * Space Complexity: O(n) , in worst case when BST is skew tree
+ * Time Complexity: O(n), n is the total number of nodes in BST
+ */
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val,Node _left,Node _right) {
+        val = _val;
+        left = _left;
+        right = _right;
+    }
+};
+*/
+
+class Solution {
+    Node first=null;
+    Node last=null;
+    public Node treeToDoublyList(Node root) {
+        if(root==null)
+            return root;
+        helper(root);
+        first.left=last;
+        last.right=first;
+        return first;
+    }
+    public void helper(Node root){
+        if(root==null)
+              return;
+        helper(root.left);
+        if(last!=null){
+            last.right=root;
+            root.left=last;
+            last=root;
+        }else{
+            first=root;
+            last=root;
+        }
+        helper(root.right);
+    }
+}
